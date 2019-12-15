4d5b609d208d975796076aa1f13fb0d42eafe9f4
Charlie Chapman <charliemchapman@gmail.com>
2019-12-15 14:18:58 -0600

    updated footer styles

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -38,7 +38,7 @@ export default class Header extends React.Component {
             <div>|</div>
             <Link to="/">Home</Link>
         </div>
-        <p>Made by Charlie Chapman in St. Louis, MO</p>
+        <p className={styles.signature}>Made by Charlie Chapman in St. Louis, MO</p>
       </div>
     )
   }
