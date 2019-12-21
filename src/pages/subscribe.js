e1ab45485c0ff0aba2b1b048ccba2b6a0cdab9b6
Charlie Chapman <charliemchapman@gmail.com>
2019-12-20 19:55:22 -0600

    removed dummy google podcasts link

--- src/pages/subscribe.js
+++ src/pages/subscribe.js
@@ -12,7 +12,6 @@ export default () => (
             <li><a href="http://feed.launchedfm.com">RSS</a></li>
             <li><a href="https://podcasts.apple.com/us/podcast/launched/id1491582246">Apple Podcast</a></li>
             <li><a href="https://open.spotify.com/show/4lVKxKrG2n4pGYST801GLs">Spotify</a></li>
-            <li><a href="/">Google Podcasts (coming soon)</a></li>
             <li><a href="https://itunes.apple.com/us/app/overcast-podcast-player/id888422857?ls=1&mt=8">Overcast</a></li>
             <li><a href="https://castro.fm/itunes/1491582246">Castro</a></li>
             <li><a href="https://pca.st/launched">Pocket Casts</a></li>
