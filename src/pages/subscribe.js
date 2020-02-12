2a9643930cb6acf0208b4cc649d9fcf12c0a1c82
Charlie Chapman <charliemchapman@gmail.com>
2020-02-12 16:43:19 -0600

    fixed overcast link

--- src/pages/subscribe.js
+++ src/pages/subscribe.js
@@ -12,7 +12,7 @@ export default () => (
             <li><a href="http://feed.launchedfm.com">RSS</a></li>
             <li><a href="https://podcasts.apple.com/us/podcast/launched/id1491582246">Apple Podcast</a></li>
             <li><a href="https://open.spotify.com/show/4lVKxKrG2n4pGYST801GLs">Spotify</a></li>
-            <li><a href="https://itunes.apple.com/us/app/overcast-podcast-player/id888422857?ls=1&mt=8">Overcast</a></li>
+            <li><a href="https://overcast.fm/itunes1491582246">Overcast</a></li>
             <li><a href="https://castro.fm/itunes/1491582246">Castro</a></li>
             <li><a href="https://pca.st/launched">Pocket Casts</a></li>
             <li><a href="https://www.stitcher.com/s?fid=491180">Stitcher</a></li>
